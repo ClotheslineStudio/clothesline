c9d48c9a56383c36071531df0d094e7362b9857a
Travis <54338858+TravisPeterson21@users.noreply.github.com>
2025-09-11 12:49:33 -0600

    Implement code changes to enhance functionality and improve performance

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,6 +1,8 @@
 export * from './components/core/Icon/index.js';
 export * from './components/core/Card/index.js';
 export * from './components/core/Divider/index.js';
+export * from './components/core/Button/index.js';
+
 
 
 
